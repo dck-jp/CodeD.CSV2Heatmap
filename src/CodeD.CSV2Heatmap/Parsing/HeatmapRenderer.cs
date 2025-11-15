66b53ae64e427e33ac9a700dc129f13f628832b6
D*isuke <dicek.am@clockahead.com>
2025-11-16 03:43:39 +0900

    style: remove stray blank line in ProcessBitmapSIMD

--- src/CodeD.CSV2Heatmap/Parsing/HeatmapRenderer.cs
+++ src/CodeD.CSV2Heatmap/Parsing/HeatmapRenderer.cs
@@ -298,7 +298,6 @@ namespace CodeD
                             }
                         }
                     }
-
                 }
                 
                 // Process remaining elements
